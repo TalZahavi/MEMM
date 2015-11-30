3e3c4f6190cc9edec46008680f9171260a7a3d6a
Tal Zahavi <tal.zahavi9@gmail.com>
2015-11-30 10:14:49 +0200

    Removed Old Code

--- BasicModel.py
+++ BasicModel.py
@@ -14,10 +14,11 @@ class BasicTrainer:
         self.trigram_tag_dict = dict()
 
         self.features = dict()
-        # self.features_by_index = dict()
         self.num_his_per_feature = dict()
 
         self.calculated_features = dict()
+
+        self.first_sum_grad_vector = []
 
     # Go over the training data and find all the (history,tag) tuples
     def get_history_tag_tuples(self):
@@ -123,7 +124,6 @@ class BasicTrainer:
                 if tag_to_word_dict[tag] > 1:
                     if (word, tag) not in self.features:
                         self.features[(word, tag)] = counter
-                        # self.features_by_index[counter] = ((word, tag), 100)
                         self.num_his_per_feature[counter] = tag_to_word_dict[tag]
                         counter += 1
                         self.num_features += 1
@@ -134,7 +134,6 @@ class BasicTrainer:
                 if tag_to_tag_dict[tag_minus] > 1:
                     if (tag_minus, tag) not in self.features:
                         self.features[(tag_minus, tag)] = counter
-                        # self.features_by_index[counter] = ((tag_minus, tag), 104)
                         self.num_his_per_feature[counter] = tag_to_tag_dict[tag_minus]
                         counter += 1
                         self.num_features += 1
@@ -145,68 +144,16 @@ class BasicTrainer:
                 if two_tag_to_tag_dict[(tag_minus2, tag_minus)] > 1:
                     if ((tag_minus2, tag_minus), tag) not in self.features:
                         self.features[((tag_minus2, tag_minus), tag)] = counter
-                        # self.features_by_index[counter] = (((tag_minus2, tag_minus), tag), 103)
                         self.num_his_per_feature[counter] = two_tag_to_tag_dict[(tag_minus2, tag_minus)]
                         counter += 1
                         self.num_features += 1
 
-    # TRY 1
+    # FUNCTION L(V)
     ################################################################
 
-    # Return the result for v-dot-f on a given (history,tag) tuple
-    def calculate_v_dot_f_for_tuple(self, data_tuple, v_vector):
-        result = 0
-
-        history = data_tuple[0]
-        word_tag = data_tuple[1]
-        word_index = history[3]
-        split_sentence = (history[2]).split()
-        word = split_sentence[word_index]
-        tag_minus = history[1]
-        tag_minus2 = history[0]
-
-        if (word, word_tag) in self.features:
-            result += v_vector[self.features[(word, word_tag)]]
-        if (tag_minus, word_tag) in self.features:
-            result += v_vector[self.features[(tag_minus, word_tag)]]
-        if ((tag_minus2, tag_minus), word_tag) in self.features:
-            result += v_vector[self.features[((tag_minus2, tag_minus), word_tag)]]
-
-        return result
-
-    # Calculate the first sum of L(v)
-    def func_l_part_one(self, v_vector):
-        result = 0
-        for data_tuple in self.history_tag_tuples:
-            result += self.calculate_v_dot_f_for_tuple(data_tuple, v_vector)
-        return result
-
-    # Calculate the second sum of L(v)
-    def func_l_part_two(self, v_vector):
-        total_result = 0
-        for data_tuple in self.history_tag_tuples:
-            history = data_tuple[0]
-            features_on_tuples_array = []
-
-            for tag in self.tags:
-                features_on_tuples_array.append(self.calculate_v_dot_f_for_tuple((history, tag), v_vector))
-
-            exp_arr = np.exp(features_on_tuples_array)
-            total_result += np.log2(sum(exp_arr))
-
-        return total_result
-
-    def func_l(self, v_vector):
-        a = self.func_l_part_one(v_vector)
-        b = self.func_l_part_two(v_vector)
-        return a-b
-
-    # END TRY 1
-    #################################################################
-
-    # TRY 2
-    ################################################################
-
+    # Calculation before the v_dot_f function
+    # Fill a dict, that : (history_tag) -> (num features that apply for him)
+    # Need to preform only once
     def calculate_all_v_dot_f_for_tuple(self):
         for data_tuple in self.history_tag_tuples:
             for word_tag in self.tags:
@@ -229,6 +176,7 @@ class BasicTrainer:
 
                 self.calculated_features[(history, word_tag)] = temp_arr
 
+    # Calculate v_dot_f for a given tuple and a vector v
     def calculate_v_dot_f_for_tuple2(self, data_tuple, v_vector):
         result = 0
         features_num_arr = self.calculated_features[data_tuple]
@@ -236,12 +184,14 @@ class BasicTrainer:
             result += v_vector[index]
         return result
 
+    # Calculate the first sum of the function L(v) (for a given vector)
     def func_part1(self, v_vector):
         result = 0
         for data_tuple in self.history_tag_tuples:
             result += self.calculate_v_dot_f_for_tuple2(data_tuple, v_vector)
         return result
 
+    # Calculate the second sum of the function L(v) (for a given vector)
     def func_part2(self, v_vector):
         total_result = 0
         for data_tuple in self.history_tag_tuples:
@@ -256,79 +206,40 @@ class BasicTrainer:
 
         return total_result
 
+    # Calculate the function L(v) for a given vector
     def func_l_new(self, v_vector):
         a = self.func_part1(v_vector)
         b = self.func_part2(v_vector)
         return a-b
 
-    # END TRY 2
+    # END FUNCTION L(V)
     ##################################################################
 
     # Gradient
     ##################################################################
 
-    def calculate_specific_gradient_first_sum(self, v_vector, index):
-        result = 0
-        feature_tuple = self.features_by_index[index]
+    # Get the first sum of the gradient v
+    # Need to preform only once (the function doesnt depend on v)
+    def get_gradient_first_sum_vector(self):
+        for index in range(0, self.num_features):
+            self.first_sum_grad_vector.append(self.num_his_per_feature[index])
 
-        for data_tuple in self.history_tag_tuples:
-
-            history = data_tuple[0]
-            word_tag = data_tuple[1]
-            word_index = history[3]
-            split_sentence = (history[2]).split()
-            word = split_sentence[word_index]
-            tag_minus = history[1]
-            tag_minus2 = history[0]
-
-            if feature_tuple[1] == 100:
-                feature_data = feature_tuple[0]
-                feature_word = feature_data[0]
-                feature_word_tag = feature_data[1]
-
-                if feature_word == word and feature_word_tag == word_tag:
-                    result += 1
-
-            if feature_tuple[1] == 103:
-                feature_data = feature_tuple[0]
-                feature_tag = feature_data[1]
-                tags_tuple = feature_data[0]
-                feature_tag_minus2 = tags_tuple[0]
-                feature_tag_minus = tags_tuple[1]
-
-                if feature_tag_minus2 == tag_minus2 and feature_tag_minus == tag_minus and feature_tag == word_tag:
-                    result += 1
-
-            if feature_tuple[1] == 104:
-                feature_data = feature_tuple[0]
-                feature_tag_minus = feature_data[0]
-                feature_tag = feature_data[1]
-
-                if feature_tag_minus == tag_minus and feature_tag == word_tag:
-                    result += 1
-
-        return result
-
+    # Calculate the second sum of the gradient for a specific location
     def calculate_specific_gradient_second_sum(self, v_vector, index):
         result = 0
         return result
 
+    # Calculate a specific location in the gradient
     def calculate_specific_gradient(self, v_vector, index):
-        a = self.calculate_specific_gradient_first_sum(v_vector, index)
+        a = sum(self.first_sum_grad_vector)
         b = self.calculate_specific_gradient_second_sum(v_vector, index)
         return a-b
 
+    # Get a gradient vector for a given v
     def get_gradient_vector(self, v_vector):
         grad_vector = np.ones(shape=self.num_features)
-        for index in range(0, 10):
-            grad_vector[index] = self.calculate_specific_gradient(v_vector, index)
-        return grad_vector
-
-    def get_gradient_first_sum_vector(self):
-        #grad_vector = np.ones(shape=self.num_features, dtype=int)
-        grad_vector = []
         for index in range(0, self.num_features):
-            grad_vector.append(self.num_his_per_feature[index])
+            grad_vector[index] = self.calculate_specific_gradient(v_vector, index)
         return grad_vector
 
 
@@ -344,29 +255,17 @@ print('Removing unfrequented features...')
 x.get_frequented_features()
 print('After optimization, only ' + str(x.num_features) + ' features left\n')
 
-# print('Init v vector,')
-# v = np.ones(shape=x.num_features, dtype=int)
-# print('Calculating function L(v)...')
-# res = minimize(x.func_l, x0=v, method='L-BFGS-B')
-# print('The result is ' + str(res))
-
 print('Calculate features on all (history,tag) options...')
 x.calculate_all_v_dot_f_for_tuple()
 print('Done calculating all possible features!\n')
 
-
 v = np.ones(shape=x.num_features)
-# print('Calculating function L(v)...')
+x.get_gradient_first_sum_vector()  # Need to calculate only one time
+print('Lets try to get a gradient vector')
+y = x.get_gradient_vector(v)
+
 # res = fmin_l_bfgs_b(x.func_l_new, x0=v_vector1, approx_grad=1)
 # print('The result is ' + str(res))
-
-# print('Lets get gradient vector')
-# x.get_gradient_vector(v)
-
-print('Lets get the first sum vector of the gradient (need to calculate only one time)')
-grad = x.get_gradient_first_sum_vector()
-print(grad)
-
 
 
 print('\nFROM BEGINNING TO NOW ONLY IN ' + str(datetime.now() - startTime) + ' SECONDS!')
