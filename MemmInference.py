afa4414226f17a240301b93ec441761f7259ca27
Tal Zahavi <tal.zahavi9@gmail.com>
2015-12-02 15:56:38 +0200

    More viterbi work

--- MemmInference.py
+++ MemmInference.py
@@ -7,14 +7,15 @@ class MemmInference:
         self.features = dict()  # For a given feature number, return the feature data
         self.tags = dict()  # All possible tags
 
-
+    # Load the data that was learned before
     def load_data(self):
         self.v_vector = np.load('opt_v.npy')
         # TODO: Load Features Dict From Basic Model
         # TODO: Load Tags Set From Basic Model
 
+    # Get all the possible tags at a position in the sentence
     def get_possible_tags_at_location(self, index):
-        if index < 1:
+        if index < 0:
             return ['*']
         else:
             return self.tags
@@ -23,6 +24,7 @@ class MemmInference:
         pass
         # TODO: FILL IN
 
+    # The main part of the viterbi algorithm
     def get_max_pi_and_arg_max_tag(self):
         return (0,'')
         # TODO: FILL IN!
@@ -38,47 +40,40 @@ class MemmInference:
         #                 max_prob = temp_prob
         #                 max_tag = t
 
+    # Used for finding the last two tags in the sentence
+    def get_max_tags_tuple(self, vit_dict):
+        max_pi = 0
+        result_tuple = ('', '')
+        for (u, v) in vit_dict:
+            if vit_dict[(u, v)][0] >= max_pi:
+                max_pi = vit_dict[(u, v)][0]
+                result_tuple = (u, v)
+        return result_tuple
 
     # Using VITERBI ALGORITHM
     def sentence_inference(self, sentence, v_vec):
         viterbi_dict = dict()
-        #  Init
+        viterbi_dict[-1] = {('*', '*'): (1, '*')}
 
-        for k in range(1, len(sentence)+1):
+        for k in range(0, len(sentence)):
             temp_dict = dict()  # (u,v) -> (pi,bp)
 
             for u in self.get_possible_tags_at_location(k-1):
                 for v in self.get_possible_tags_at_location(k):
-                    #TODO : HERE NEED TO FILL PAI AND BP
                     viterbi_tuple = self.get_max_pi_and_arg_max_tag()
                     temp_dict[(u, v)] = viterbi_tuple
 
             viterbi_dict[k] = temp_dict
 
         sentence_tags = []
+
         # Finding t_n-1 t_n tags
+        tags_tuple = self.get_max_tags_tuple(viterbi_dict[len(sentence)-1])
+        sentence_tags[len(sentence)-1] = tags_tuple[1]
+        sentence_tags[len(sentence)-2] = tags_tuple[0]
 
         # Finding the rest of the tags
-        for k in range(len(sentence)-2, 1, -1):
-            (pi, bp) = viterbi_dict[k+2][(sentence_tags[k+1],sentence_tags[k+2])]
+        for k in range(len(sentence)-3, 0, -1):
+            (pi, bp) = viterbi_dict[k+2][(sentence_tags[k+1], sentence_tags[k+2])]
             sentence_tags[k] = bp
-
-
-
-    # def temp(self):
-    #
-    # viterbi_dict[0] = {('*', '*'): (1, '*')}
-    #
-    # # finding tn-1 tn
-    # n_dic = viterbi_dict[n]
-    # max_n_prob = 0
-    # max_n_tags = ('', '')
-    # for tuple_tags in n_dic:
-    #     tuple_values = n_dic[tuple_tags]
-    #     if max_n_prob <= tuple_values[0]:
-    #         max_n_prob = tuple_values[0]
-    #         max_n_tags = tuple_tags
-    # sentence_tags[n-1] = max_n_tags[0]
-    # sentence_tags[n] = max_n_tags[1]
-
 
