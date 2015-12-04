327732a5feaebbee46073d28530b0032de3115f2
Tal Zahavi <tal.zahavi9@gmail.com>
2015-12-04 12:11:13 +0200

    Start working on improved..not working yet

--- Improved/ImprovedModelFeature.py
+++ Improved/ImprovedModelFeature.py
@@ -1,5 +1,34 @@
-import numpy as np
-import pickle
+feature_105_dict = dict()
+feature_101_dict = dict()
+feature_102_dict = dict()
+
+
+# Auxiliary function - check if a given word ends with the given suffix (num_char is the length of the suffix)
+def check_suffix(word, num_char, suffix):
+    if word == '':
+        return False
+    word_chars = list(word)
+    suffix_index = num_char
+    for i in range(len(word)-1, len(word)-num_char-1, -1):
+        if word_chars[i] != suffix[suffix_index-1]:
+            return False
+        suffix_index -= 1
+    return True
+
+
+# Auxiliary function - check if a given word starts with the given prefix (num_char is the length of the prefix)
+def check_prefix(word, num_char, prefix):
+    if word == '':
+        return False
+    word_chars = list(word)
+    prefix_index = 0
+    for i in range(0, num_char):
+        print('checking ' + word_chars[i])
+        print('with ' + prefix[prefix_index])
+        if word_chars[i] != prefix[prefix_index]:
+            return False
+        prefix_index += 1
+    return True
 
 
 # Fill the features dicts according to the seen data
@@ -8,15 +37,48 @@ def fill_features_dicts(self):
         word_index = history[3]
         split_sentence = (history[2]).split()
         word = split_sentence[word_index]
+        tag_minus2 = history[0]
+        tag_minus = history[1]
 
         add_word_tag_to_dict(self, word, word_tag)
-        add_bigram_tag_to_dict(self, history[1], word_tag)
-        add_trigram_tag_to_dict(self, history[0], history[1], word_tag)
+        add_bigram_tag_to_dict(self, tag_minus, word_tag)
+        add_trigram_tag_to_dict(self, tag_minus2, tag_minus, word_tag)
+
+        if check_suffix(word, 3, 'ing') and word_tag == 'VBG':
+            add_suffix_ing_to_dict(word)
+        if check_prefix(word, 3, 'pre') and word_tag == 'NN':
+            add_prefix_pre_to_dict(word)
+
+
+# Fill the 101 feature with a new seen feature
+# If already saw the feature, update the counter
+def add_suffix_ing_to_dict(word):
+    if word in feature_101_dict:
+        feature_101_dict[word] += 1
+    else:
+        feature_101_dict[word] = 1
+
+
+# Fill the 102 feature with a new seen feature
+# If already saw the feature, update the counter
+def add_prefix_pre_to_dict(word):
+    if word in feature_102_dict:
+        feature_102_dict[word] += 1
+    else:
+        feature_102_dict[word] = 1
+
+
+# Fill the 105 feature with a new seen feature
+# If already saw the feature, update the counter
+def add_tag_to_dict(tag):
+    if tag in feature_105_dict:
+        feature_105_dict[tag] += 1
+    else:
+        feature_105_dict[tag] = 1
 
 
 # Fill the 100 feature with a new seen feature
 # If already saw the feature, update the counter
-
 def add_word_tag_to_dict(self, word, word_tag):
     if word in self.word_to_tags_dict:
         tags_per_word_dict = self.word_to_tags_dict[word]
@@ -103,41 +165,28 @@ def get_frequented_features(self):
                     counter += 1
                     self.num_features += 1
 
+    for tag in feature_105_dict:
+        if feature_105_dict[tag] > 7:
+            if (tag, tag) not in self.features:
+                self.features[(tag, tag)] = counter
+                self.num_his_per_feature[counter] = feature_105_dict[tag]
+                counter += 1
+                self.num_features += 1
 
-# Calculation before the v_dot_f function
-# Fill a dict, that : (history_tag) -> (num features that apply for him)
-# Need to preform only once
-def calculate_all_dot_f_for_tuple(self):
-    for data_tuple in self.history_tag_tuples:
-        for word_tag in self.tags:
+    for word in feature_101_dict:
+        if feature_101_dict[word] > 3:
+            if (word, 'ing') not in self.features:
+                self.features[(word, 'ing')] = counter
+                self.num_his_per_feature[counter] = feature_101_dict[word]
+                counter += 1
+                self.num_features += 1
 
-            temp_arr = []
-
-            history = data_tuple[0]
-            word_index = history[3]
-            split_sentence = (history[2]).split()
-            word = split_sentence[word_index]
-            tag_minus = history[1]
-            tag_minus2 = history[0]
-
-            if (word, word_tag) in self.features:
-                temp_arr.append(self.features[(word, word_tag)])
-            if (tag_minus, word_tag) in self.features:
-                temp_arr.append(self.features[(tag_minus, word_tag)])
-            if ((tag_minus2, tag_minus), word_tag) in self.features:
-                temp_arr.append(self.features[((tag_minus2, tag_minus), word_tag)])
-
-            self.calculated_features[(history, word_tag)] = temp_arr
-
-            for num_feature in temp_arr:
-                if num_feature in self.tuples_per_feature:
-                    self.tuples_per_feature[num_feature].append((history, word_tag))
-                else:
-                    self.tuples_per_feature[num_feature] = [(history, word_tag)]
+    for word in feature_102_dict:
+        if feature_102_dict[word] > 3:
+            if (word, 'pre') not in self.features:
+                self.features[(word, 'pre')] = counter
+                self.num_his_per_feature[counter] = feature_102_dict[word]
+                counter += 1
+                self.num_features += 1
 
 
-def save_data_to_files(self):
-    pickle.dump(self.features, open("basic_features_dict.p", "wb"), protocol=2)
-    pickle.dump(self.tags, open("basic_tags.p", "wb"), protocol=2)
-    pickle.dump(self.word_to_freq_tags_dict, open("basic_freq_tags.p", "wb"), protocol=2)
-    np.save('basic_opt_v', self.v_vec)
